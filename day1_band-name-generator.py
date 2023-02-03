1455db84abbe8e00f48ac941ab9638dec3ab338e
Michael Primavera <pastasauce59@gmail.com>
2023-02-02 19:45:27 -0500

    Initial commit

---
+++ day1_band-name-generator.py
@@ -0,0 +1,9 @@
+print('Hello, welcome to the Band Name Generator!')
+
+user_city = input("What city did you grow up in?\n")
+
+user_pet = input("What is the name of your pet?\n")
+
+band_name = user_city + " " + user_pet
+
+print('Your band name could be: ' + band_name)
